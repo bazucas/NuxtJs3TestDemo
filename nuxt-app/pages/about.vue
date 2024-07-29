d9ff66c8adfa4229472b768291a182fabd4d0e52
Luís Inácio <lrs.inacio@gmail.com>
2024-07-29 12:04:51 +0100

    Routing middleware with pre-configured limitations or redirects

--- nuxt-app/pages/about.vue
+++ nuxt-app/pages/about.vue
@@ -3,6 +3,14 @@
         <h1>About page</h1>
         <NuxtLink to="/">Home (NuxtLink)</NuxtLink> <br>
         <a href="/">Home (anchor)</a>
+        <!-- <h1><NuxtLink to="/Users/1">users/1</NuxtLink></h1>    
+        <h1><NuxtLink to="/Users/2">users/2</NuxtLink></h1>    
+        <h1><NuxtLink to="/Users/3">users/3</NuxtLink></h1>    
+        <h1><NuxtLink to="/Users/84">users/4</NuxtLink></h1>     -->
+        <h1><a href="/users/1">users/1</a></h1>    
+        <h1><a href="/users/2">users/2</a></h1>    
+        <h1><a href="/users/3">users/3</a></h1>    
+        <h1><a href="/users/84">users/4</a></h1>  
     </div>
 </template>
 
--- nuxt-app/pages/index.vue
+++ nuxt-app/pages/index.vue
@@ -7,7 +7,6 @@
         <h1><NuxtLink to="/PageMetaMiddleware/">PageMeta</NuxtLink></h1>    
         <h1><NuxtLink to="/NuxtRouteMiddleware/">NuxtRoute</NuxtLink></h1>    
         <h1><NuxtLink to="/GlobalRouteMiddleware/">GlobalRoute</NuxtLink></h1>    
-        <h1><NuxtLink to="/Users/1">users/1</NuxtLink></h1>    
     </div>
 </template>
 
