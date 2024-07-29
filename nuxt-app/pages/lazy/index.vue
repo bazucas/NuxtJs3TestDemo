1f2f98a768f0af645a555631590308c7207df2a5
Luís Inácio <lrs.inacio@gmail.com>
2024-07-29 19:18:40 +0100

    Lazy load Welcome component inside lazy/index.vue by prefixing it with LazyWelcome

---
+++ nuxt-app/pages/lazy/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+      <LazyWelcome v-if="show"/>
+      <button class="w-56 h-12 bg-blue-500 text-white font-bold py-2 px-4 rounded-lg shadow-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition duration-300" 
+      @click="show = !show">Show Welcome message
+    </button>
+    </div>
+  </template>
+  
+  <script setup>
+  const show = ref(false)
+  </script>
+  
