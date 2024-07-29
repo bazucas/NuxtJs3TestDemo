902d3a10f8d96e7244765cb120d19b37129ad7e6
Luís Inácio <lrs.inacio@gmail.com>
2024-07-29 12:44:39 +0100

    Global middleware to limit access to any page based on url param

--- nuxt-app/plugins/my-plugin.js
+++ nuxt-app/plugins/my-plugin.js
@@ -1,18 +1,24 @@
 export default defineNuxtPlugin( () => {
-    addRouteMiddleware( (to, from) => {
+    addRouteMiddleware((to, from) => {
         if(to.path === '/database') {
             return abortNavigation('Forbidden page')
         }
     })
 
-    addRouteMiddleware('denyLuis', (to, from) => {
+    // addRouteMiddleware('denyLuis', (to, from) => {
+    //     const name = to.params.name;
+    //     if (name && name.toLowerCase() === 'luis') {
+    //         return abortNavigation('The name Luis is not allowed');
+    //     }
+    // });
+
+    addRouteMiddleware((to)=> {
+        console.log('Global middleware')
+
         const name = to.params.name;
         if (name && name.toLowerCase() === 'luis') {
             return abortNavigation('The name Luis is not allowed');
         }
-    });
 
-    addRouteMiddleware(() => {
-        console.log('Global middleware')
     }, {global: true})
 })
