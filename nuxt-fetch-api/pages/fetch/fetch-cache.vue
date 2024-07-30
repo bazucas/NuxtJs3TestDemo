16aa92129a6ee9e1249a64cf5c7d1a5a7a54fe59
Luís Inácio <lrs.inacio@gmail.com>
2024-07-30 22:02:17 +0100

    getCachedData for caching with explanation

--- nuxt-fetch-api/pages/fetch/fetch-cache.vue
+++ nuxt-fetch-api/pages/fetch/fetch-cache.vue
@@ -6,10 +6,16 @@ const id = ref(1);
 const nuxtApp = useNuxtApp()
 
 const { data: product } = await useFetch(() => `https://fakestoreapi.com/products/${id.value}`, {
+  // The getCachedData function checks for cached data before making a new API request.
+  // It tries to retrieve data from two sources:
+  // 1. nuxtApp.payload.data: This contains data that was injected into the client
+  //    during the server-side rendering process. This data might include pre-fetched
+  //    API responses that were provided during SSR to avoid duplicate requests on the client.
+  // 2. nuxtApp.static.data: This contains static data generated at build time
+  //    for static sites. This data is pre-fetched and included in the static site output,
+  //    ensuring that the data does not change between builds.
   getCachedData: key => {
-    return nuxtApp.payload.data
-    [key] || nuxtApp.static.data
-    [key]
+    return nuxtApp.payload.data[key] || nuxtApp.static.data[key]
   }
 });
 
