44149a100632592b44f990e92c0c6c8e7e822a27
Luís Inácio <lrs.inacio@gmail.com>
2024-07-30 17:19:15 +0100

    Immediate and Execute

---
+++ nuxt-fetch-api/pages/fetch/fetch-immediate.vue
@@ -0,0 +1,19 @@
+<script setup>
+// refresh/execute: a function that can be used to refresh the data returned by the handler function.
+const {data: product, execute} = await useFetch('https://fakestoreapi.com/products/1', {
+  immediate: false // immediate: when set to false, will prevent the request from firing immediately. (defaults to true)
+  })
+
+</script>
+
+<template>
+  <div>
+    <p>{{ product }}</p>
+    <button @click="execute">Fetch Data</button>
+    <br><br>
+  </div>
+</template>
+
+<style>
+
+</style>
