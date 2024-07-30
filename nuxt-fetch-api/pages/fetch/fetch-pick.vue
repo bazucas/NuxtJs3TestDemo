bcbd5ce7a113ebad3a2e923310ba31c43334ba69
Luís Inácio <lrs.inacio@gmail.com>
2024-07-30 18:00:19 +0100

    Pick returns a specific property(ies) from the request pipeline

---
+++ nuxt-fetch-api/pages/fetch/fetch-pick.vue
@@ -0,0 +1,19 @@
+<script setup>
+const {data: product, execute} = await useFetch('https://fakestoreapi.com/products/1', {
+    pick: ["title"]
+  })
+
+</script>
+
+<template>
+  <div>
+    <p>{{ product }}</p>
+    <br>
+    <button @click="execute">Fetch Data</button>
+    <br><br>
+  </div>
+</template>
+
+<style>
+
+</style>
