b8ae31cd5ddf676125d052023918551023bef6ad
Luís Inácio <lrs.inacio@gmail.com>
2024-07-30 22:32:34 +0100

    Fetch status during before during and after requests

---
+++ nuxt-fetch-api/pages/fetch/fetch-status.vue
@@ -0,0 +1,25 @@
+<script setup>
+  const {data: product, pending, refresh, execute, error, status} = await useFetch(() => {
+    return 'https://fakestoreapi.com/products/1'
+  }, {
+    immediate: false
+  })
+
+  console.log(status.value) // status => idle
+
+  setTimeout(() => {
+    execute()
+    console.log(status.value) // status => pending
+
+    setTimeout(() => {
+      console.log(status.value) // status => success
+      }, 3000)
+    }, 3000)
+
+</script>
+
+<template>
+  <div>
+
+  </div>
+</template>
