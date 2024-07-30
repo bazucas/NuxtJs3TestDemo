557f6ffec07259c41f3e6e8953ab507c7bb39e21
Luís Inácio <lrs.inacio@gmail.com>
2024-07-30 16:25:15 +0100

    Get with query params

---
+++ nuxt-fetch-api/pages/fetch/fetch-stocks.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+      {{ stocks }}
+    </div>
+  </template>
+  
+  <script setup>
+    const apiKey = 'SV90TOORA7CK3RJZ'
+    const {data: stocks} = await useFetch('/query', {
+      baseURL: 'https://www.alphavantage.co',
+    //   query: { // same thing as params: {}
+      params: {
+        function: 'TIME_SERIES_INTRADAY',
+        symbol: 'IBM',
+        interval: '5min',
+        apikey: apiKey
+      },
+      timeout: 5000 // if the response doesn't arrive in less than 5s, it will be closed
+    })
+  </script>
+  
+  <style>
+  
+  </style>
+  
+  
+  
