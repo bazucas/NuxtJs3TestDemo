1e1ce05ef4e07764425e72d56c5ae7b96f2074b1
Luís Inácio <lrs.inacio@gmail.com>
2024-07-30 16:32:06 +0100

    Fetch api in the clientside by disabling server: false

---
+++ nuxt-fetch-api/pages/fetch/fetch-post-clientside.vue
@@ -0,0 +1,18 @@
+<script setup>
+  const {data: product} = await useFetch('https://fakestoreapi.com/products/1', {
+    server: false // this flag will disable nuxt's server side to send the page content without the API call, this will be done in the client side
+  })
+  console.log(product)
+</script>
+
+<template>
+  <div>
+    {{ product }}
+    <br><br>
+  </div>
+</template>
+
+
+<style>
+
+</style>
