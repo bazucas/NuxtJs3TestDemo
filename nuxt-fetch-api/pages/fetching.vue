0b7b0a8e5525380aba85ea1c013f896e1297e03e
Luís Inácio <lrs.inacio@gmail.com>
2024-07-30 13:45:34 +0100

    Basic await useFetch with closure problem fix

--- nuxt-fetch-api/pages/fetching.vue
+++ nuxt-fetch-api/pages/fetching.vue
@@ -1,12 +1,30 @@
 <template>
     <div>
         {{  products }}
+        <br><br>
+        <button @click="fetchProduct">Display new product</button>
     </div>
   </template>
   
   <script setup>
-    const {data: products} = await useFetch('https://fakestoreapi.com/products') // {data: products} -> destructuring + new name assign
+  const id = ref(1)
+  // const {data: products} = await useFetch(`https://fakestoreapi.com/products/${id}`);
+  
+  // The following line doesn't work because of closure, as the variable is stored globally
+  // const {data: products, refresh} = await useFetch(`https://fakestoreapi.com/products/${id.value}`);
+  
+  // The solution is to use a function to return the URL, so the variable is evaluated dynamically
+  const { data: products, refresh } = await useFetch(() => `https://fakestoreapi.com/products/${id.value}`);
     console.log(products.value)
+
+    const fetchProduct = async() => {
+        ++id.value
+        console.log("ID 👌👌")
+        console.log(id.value)
+        await refresh()
+        console.log(products.value)
+    }
+
   </script>
   
 
