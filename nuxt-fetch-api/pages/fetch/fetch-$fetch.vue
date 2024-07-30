fa4e1e4a628e8c0a8bde82257e2726a9f9062454
Luís Inácio <lrs.inacio@gmail.com>
2024-07-31 00:13:20 +0100

    Fetch, $Fetch and useFetch comparison, useFetch is a lot better with composition api and totally reactive

---
+++ nuxt-fetch-api/pages/fetch/fetch-$fetch.vue
@@ -0,0 +1,20 @@
+<script setup>
+   
+    // *Basic* native function from javascript, very limited, we need to take care of the promises, errors, callbacks, etc
+    // calls twice this API both in the server and client
+    // const toDo1 = await fetch('https://jsonplaceholder.typicode.com/todos/1') 
+    
+    // *Good* nuxtjs specific function, more advanced than fetch from js
+    // calls twice this API both in the server and client
+    // const toDo2 = await $fetch('https://jsonplaceholder.typicode.com/todos/1') 
+        
+     // *Great* Totally reactive and integrated with nuxt and vue with Composition API
+    const {data: toDo, pending, error} = await useFetch('https://jsonplaceholder.typicode.com/todos/1');
+</script>
+
+<template>
+  <div>
+    {{ toDo }}
+    <br><br>
+  </div>
+</template>
