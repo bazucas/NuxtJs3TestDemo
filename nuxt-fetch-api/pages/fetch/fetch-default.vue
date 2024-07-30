52c00803c20f3829d824b3733671b5f6f86ec19f
Luís Inácio <lrs.inacio@gmail.com>
2024-07-30 17:54:32 +0100

    Inject a default value into the response pipeline even if the API has not yet responded using default

---
+++ nuxt-fetch-api/pages/fetch/fetch-default.vue
@@ -0,0 +1,24 @@
+<script setup>
+const {data: product, execute} = await useFetch('https://fakestoreapi.com/products/1', {
+  immediate: false,
+  server: false,
+  default: (product) => {
+    product = "No data available"
+    return product
+  }
+  })
+
+</script>
+
+<template>
+  <div>
+    <p>{{ product }}</p>
+    <br>
+    <button @click="execute">Fetch Data</button>
+    <br><br>
+  </div>
+</template>
+
+<style>
+
+</style>
