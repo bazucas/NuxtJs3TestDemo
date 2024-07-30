0033bbb4e4ead6fcaf03acf13fe2dba0d7531dab
Luís Inácio <lrs.inacio@gmail.com>
2024-07-30 12:08:50 +0100

    TailwindCss fix

--- nuxt-fetch-api/nuxt.config.ts
+++ nuxt-fetch-api/nuxt.config.ts
@@ -1,5 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from 'nuxt/config'
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: false },
+  css: ['~/assets/css/main.scss'], // Importar o arquivo SCSS principal
+  //modules: ['@nuxtjs/tailwindcss'], //another way of importing npm i nuxtjs/tailwindcss
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {}
+    },
+  },
+  app: {
+    pageTransition: {
+      name: "page",
+      mode: "out-in"
+    },
+    layoutTransition: {
+      name: "page",
+      mode: "out-in"
+    }
+  }
 })
