96acf13e14c64c788750cd1df7f6a218aabff2c9
Luís Inácio <lrs.inacio@gmail.com>
2024-07-30 22:33:31 +0100

    Fetch failed request (ex. bad url) shows error message with http error response

---
+++ nuxt-fetch-api/pages/fetch/fetch-error.vue
@@ -0,0 +1,14 @@
+<script setup>
+  const {data: product, pending, refresh, execute, error, status} = await useFetch(() => {
+    return 'https://fakestoreapi.com/productss/1' // bad Url, will show message
+  })
+
+</script>
+
+<template>
+  <div>
+    <p v-if="error">{{ error }}</p>
+    <p v-else>{{ product }}</p>
+    <br><br>
+  </div>
+</template>
