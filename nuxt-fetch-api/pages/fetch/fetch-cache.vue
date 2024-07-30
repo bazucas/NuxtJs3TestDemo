33d7672fff68f16a08d14d0bdb10bbc3c8fdfbf2
Luís Inácio <lrs.inacio@gmail.com>
2024-07-30 19:25:41 +0100

    Caching [not working], changing between 2 pages, on of the won't call the service again because already has the content

---
+++ nuxt-fetch-api/pages/fetch/fetch-cache.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import { useFetch } from '#app';
+
+const id = ref(1);
+const nuxtApp = useNuxtApp()
+
+const { data: product } = await useFetch(() => `https://fakestoreapi.com/products/${id.value}`, {
+  getCachedData: key => {
+    return nuxtApp.payload.data
+    [key] || nuxtApp.static.data
+    [key]
+  }
+});
+
+
+</script>
+
+<template>
+  <div>
+    <p>{{ product }}</p>
+    <NuxtLink to="/fetch/fetch-index">Index</NuxtLink>
+    <br><br>
+  </div>
+</template>
---
+++ nuxt-fetch-api/pages/fetch/fetch-index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>Fetch Home</h1>
+        <NuxtLink to="/fetch/fetch-cache">Index</NuxtLink>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
