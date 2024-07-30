c2b243567b8032cd896147bfeba3046f625df354
Luís Inácio <lrs.inacio@gmail.com>
2024-07-30 13:25:34 +0100

    Fetch working

---
+++ nuxt-fetch-api/pages/fetching.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        {{  products }}
+    </div>
+  </template>
+  
+  <script setup>
+    const {data: products} = await useFetch('https://fakestoreapi.com/products') // {data: products} -> destructuring + new name assign
+    console.log(products.value)
+  </script>
+  
+
+  <style lang="scss" scoped>
+  
+  </style>
+   
