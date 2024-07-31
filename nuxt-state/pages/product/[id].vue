42c087dec6be1671b66f4dbb0c5d194fc3b89f3e
Luís Inácio <lrs.inacio@gmail.com>
2024-07-31 15:36:22 +0100

    Throw error inside DOM, not good

---
+++ nuxt-state/pages/product/[id].vue
@@ -0,0 +1,20 @@
+<script setup>
+const route = useRoute()
+const id = route.params.id
+// const {data: product} = await useFetch(`https://fakestoreapi.com/products/${id}`)
+const {data: product} = await useFetch(`https://fakestoreapi.com/productss/${id}`) // bad URL
+
+// if(!product.value) {
+//     throw createError({ // custom error throw
+//         statusCode: 404,
+//         message: "Product not found!!!"
+//     })
+//}
+</script>
+
+<template>
+  <div>
+    <p v-if="error">{{ error }}</p>
+    <p v-else>{{ product }}</p>
+  </div>
+</template>
