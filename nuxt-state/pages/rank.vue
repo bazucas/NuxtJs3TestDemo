7499fbdeaae1030ac2b95e5ae74882000d5028ed
Luís Inácio <lrs.inacio@gmail.com>
2024-07-31 15:07:12 +0100

    In shallowRef the property is updated in JS but not on the view it won't trigger DOM update

---
+++ nuxt-state/pages/rank.vue
@@ -0,0 +1,19 @@
+<script setup>
+    const rank = useState('rank', () => {
+        return shallowRef({
+            num: 3
+        })
+    })
+
+    const changeRank = () => {
+        rank.value.num = 10
+        console.log(rank.value.num)
+    }
+</script>
+
+<template>
+  <div>
+    <h1>Rank: {{ rank }}</h1>
+    <button @click="changeRank">Change Rank</button>
+  </div>
+</template>
