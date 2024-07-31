1bceee37d012fca955cf4792fd9ca88002b94032
Luís Inácio <lrs.inacio@gmail.com>
2024-07-31 15:13:35 +0100

    Todos challenge with useState

--- nuxt-state/pages/index.vue
+++ nuxt-state/pages/index.vue
@@ -1,19 +1,10 @@
 <template>
   <div>
-    <h1>Counter: {{ counter }}</h1>
-    <br>  
-    <button @click="counter++">Increment</button>  
-
-    <br><br>
-    <NuxtLink to="/about">About page</NuxtLink>
+      <NuxtLink to="/todos">Go to Todos Page</NuxtLink>
   </div>
 </template>
 
-<script lang="ts" setup>
-    const counter = useCounter()
+<script setup>
 
 </script>
 
-<style>
-
-</style>
