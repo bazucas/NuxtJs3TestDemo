ad62fdfe95d4b71686e1b40c148e94f0aa79c594
Luís Inácio <lrs.inacio@gmail.com>
2024-07-31 13:43:21 +0100

    Tailwind config and css files

--- nuxt-state/nuxt.config.ts
+++ nuxt-state/nuxt.config.ts
@@ -1,5 +1,37 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+// nuxt.config.ts
+import { defineNuxtConfig } from 'nuxt/config'
+import type { Plugin } from 'vite'
+
+// Definição do plugin personalizado para Vite
+function customOutputPlugin(): Plugin {
+  return {
+    name: 'custom-css-plugin',
+    enforce: 'post',
+    generateBundle(options, bundle) {
+      for (const fileName in bundle) {
+        if (fileName.endsWith('.css')) {
+          bundle['custom.css'] = { ...bundle[fileName], fileName: 'custom.css' }
+          delete bundle[fileName]
+        }
+      }
+    }
+  }
+}
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: [
+    '~/assets/scss/main.scss',
+    '~/assets/css/tailwind.css'
+  ],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {}
+    },
+  },
+  vite: {
+    plugins: [customOutputPlugin()]
+  }
 })
