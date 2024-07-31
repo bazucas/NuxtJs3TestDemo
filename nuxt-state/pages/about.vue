927f5ec2a0f3d726b5ecf3dc8dd573c751a5b535
Luís Inácio <lrs.inacio@gmail.com>
2024-07-31 14:50:27 +0100

    Share states between 2 pages, it will lose content after refresh

---
+++ nuxt-state/pages/about.vue
@@ -0,0 +1,12 @@
+<script setup>
+const counter = useState('counterState', () => 1)
+</script>
+
+<template>
+  <div>
+    <h1>Counter: {{ counter }}</h1>
+    <button @click="counter++">Increment</button>
+    <br><br>
+    <NuxtLink to="/">Home page</NuxtLink>
+  </div>
+</template>
--- nuxt-state/pages/index.vue
+++ nuxt-state/pages/index.vue
@@ -1,10 +1,16 @@
 <template>
   <div>
-    
+    <h1>Counter: {{ counter }}</h1>
+    <br>  
+    <button @click="counter++">Increment</button>  
+
+    <br><br>
+    <NuxtLink to="/about">About page</NuxtLink>
   </div>
 </template>
 
 <script lang="ts" setup>
+    const counter = useState("counterState", () => 1)
 
 </script>
 
