2bef5fe0a68e28336e04ef91b197b481d7fa8f73
Luís Inácio <lrs.inacio@gmail.com>
2024-07-31 15:26:43 +0100

    Create default error 404 page

---
+++ nuxt-state/error.vue
@@ -0,0 +1,10 @@
+<script setup>
+    defineProps(['error'])
+</script>
+
+<template>
+  <div>
+    <h1>Errrooooorrr 👉 {{ error.statuscode }}</h1>
+    <h1>{{ error.message }}</h1>
+  </div>
+</template>
