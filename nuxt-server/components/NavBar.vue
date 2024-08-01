e98a29c074a550c57afe359b5cfd4eef02365373
Luís Inácio <lrs.inacio@gmail.com>
2024-08-01 11:26:20 +0100

    Initial structure with tailwind

---
+++ nuxt-server/components/NavBar.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <nav class="bg-gray-800 p-2">
+            <ul class="flex list-none m-0 p-0">
+                <li class="ml-4"><NuxtLink to="/" class="text-white no-underline font-bold">Home</NuxtLink></li>
+                <li class="ml-4"><NuxtLink to="/about" class="text-white no-underline font-bold">About</NuxtLink></li>
+                <!-- <li class="ml-4"><NuxtLink to="/services" class="text-white no-underline font-bold">Services</NuxtLink></li>
+                <li class="ml-4"><NuxtLink to="/pricing" class="text-white no-underline font-bold">Pricing</NuxtLink></li>
+                <li class="ml-4"><NuxtLink to="/contact" class="text-white no-underline font-bold">Contact</NuxtLink></li> -->
+            </ul>
+        </nav>
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ nuxt-server/pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+      <NuxtLink to="/todos">Go to Todos Page</NuxtLink>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
