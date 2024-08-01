501b1e727b09461035eeb4f76d325b64707e920a
Luís Inácio <lrs.inacio@gmail.com>
2024-08-01 12:25:20 +0100

    Another API call to greeting, returns object

---
+++ nuxt-server/pages/greeting.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      <h1>Home Page</h1>
+      <br>
+      <div>{{ response.greeting }}</div>
+      <div>{{ response.content }}</div>
+    </div>
+  </template>
+  
+  <script setup>
+    import { ref, onMounted } from 'vue'
+    import DOMPurify from 'dompurify'
+    import { useFetch } from '#imports'
+  
+    const sanitizedData = ref('')
+  
+    const { data: response, refresh } = useFetch("/api/greeting")
+  </script>
+  
