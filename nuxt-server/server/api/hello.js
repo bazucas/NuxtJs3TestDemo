12255a385593bfb4f7503aaca68509ea8b800d36
Luís Inácio <lrs.inacio@gmail.com>
2024-08-01 12:01:46 +0100

    Update API response with call counter

--- nuxt-server/server/api/hello.js
+++ nuxt-server/server/api/hello.js
@@ -1,6 +1,12 @@
+
+let counter = 0
+
 export default defineEventHandler((event) => {
+    counter++
     return `
     <H1>Hello world!</H1>
     <script>alert("You've been Hacked!!!")</script>
+    <br>
+    ${counter}
     `
 })
