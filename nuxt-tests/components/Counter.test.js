0093c2a93b2ff3795a4d925abdff946fae8e2b30
Luís Inácio <lrs.inacio@gmail.com>
2024-08-01 19:15:33 +0100

    UI tests with nuxt test utils

--- nuxt-tests/components/Counter.test.js
+++ nuxt-tests/components/Counter.test.js
@@ -1,15 +1,15 @@
-import { mount } from '@vue/test-utils'
+import { mountSuspended, toMatchInlineSnapshot } from '@nuxt/test-utils/runtime'
 import { describe, it, expect } from 'vitest'
 import Counter from './Counter'
 
 describe('Counter', () => {
   it('Increments counter when the user clicks on the button', async () => {
-    const component = mount(Counter)
+    const component = await mountSuspended(Counter)
     const button = component.find('button')
     const text = component.find('h1')
 
     expect(text.text()).toContain('Counter: 1')
     await button.trigger('click')
-    expect(text.text()).toContain('Counter: 2')
+    expect(text.text()).toContain('Counter: 2') //toContain is just better than toMatchInlineSnapshot
   })
 })
--- nuxt-tests/components/HelloWorld.test.js
+++ nuxt-tests/components/HelloWorld.test.js
@@ -1,10 +1,10 @@
-import { mount } from '@vue/test-utils'
+import { mountSuspended,  } from '@nuxt/test-utils/runtime'
 import HelloWorld from './HelloWorld'
 import { describe, it, expect } from 'vitest'
 
 describe('HelloWorld', () => {
-  it('displays Hello World', () => {
-    const component = mount(HelloWorld)
-    expect(component.text()).toContain('Hello World')
+  it('displays Hello World', async () => {
+    const component = await mountSuspended(HelloWorld)
+    expect(component.text()).toMatchInlineSnapshot('"Hello World"') // toContains is better because of dual quotation
   })
 })
