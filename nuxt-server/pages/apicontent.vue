27b546e85319e8b9f1f811b43c6053bac7741fd8
Luís Inácio <lrs.inacio@gmail.com>
2024-08-01 13:02:18 +0100

    Send information and retrieve in both client and server side at the same time

---
+++ nuxt-server/pages/apicontent.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+      <h1>Home Page</h1>
+      <br>
+      <div v-html="sanitizedData"></div>
+    </div>
+  </template>
+  
+  <script setup>
+    import { ref, onMounted } from 'vue'
+    import DOMPurify from 'dompurify'
+    import { useFetch } from '#imports'
+  
+    const sanitizedData = ref('')
+  
+    const { data: responseData, refresh } = useFetch("/api/hello")
+  
+    // Watch for changes in responseData or refresh to fetch new data
+    onMounted(() => {
+      if (responseData.value) {
+        sanitizedData.value = DOMPurify.sanitize(responseData.value, { USE_PROFILES: { html: true } })
+      }
+    })
+  </script>
+  
