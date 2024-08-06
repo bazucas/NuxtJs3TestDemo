e8d048151d435c4ac0b8c022f54f77894fdd77dc
Luís Inácio <lrs.inacio@gmail.com>
2024-08-06 13:35:43 +0100

    Using docus to document my application

--- randoms/nuxt.config.ts
+++ randoms/nuxt.config.ts
@@ -1,16 +1,12 @@
 export default defineNuxtConfig({
   // https://github.com/nuxt-themes/docus
   extends: ['@nuxt-themes/docus'],
+
   devtools: { enabled: true },
 
   runtimeConfig: {
     apiNinjaKey: process.env.apiNinjaKey
-  }
-})
-function defineNuxtConfig(arg0: {
-  // https://github.com/nuxt-themes/docus
-  extends: string[]; devtools: { enabled: boolean; }; runtimeConfig: { apiNinjaKey: string; };
-}) {
-  throw new Error("Function not implemented.");
-}
+  },
 
+  compatibilityDate: '2024-08-06'
+})
--- randoms/server/api/posts/[id].get.js
+++ randoms/server/api/posts/[id].get.js
@@ -1,6 +1,7 @@
 export default defineEventHandler(async event => {
     const id = getRouterParam(event, 'id')
-    const url = `https://jsonplaceholder.typicode.com/posts/${id}`
+    const {id2} = getQuery(event)
+    const url = `https://jsonplaceholder.typicode.com/posts/${id2}`
     const data = await $fetch(url)
     return data
 })
