30d17ef9b23e3e9708060608178be70aba5bcfb7
Luís Inácio <lrs.inacio@gmail.com>
2024-08-17 22:41:33 +0100

    Sell fix

--- shopiverse/pages/sell.vue
+++ shopiverse/pages/sell.vue
@@ -8,7 +8,7 @@
             </div>
             <div class="mb-5">
                 <label for="small-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Product Category</label>
-                <input v-model="productCategory"ype="text" id="small-input" class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <input v-model="productCategory" type="text" id="small-input" class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
             </div>
             <div class="mb-5">
                 <label for="small-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Product Price</label>
