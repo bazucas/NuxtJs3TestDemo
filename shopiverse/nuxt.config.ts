933826f99715833908043f7a77e610a166b32007
Luís Inácio <lrs.inacio@gmail.com>
2024-08-17 21:57:54 +0100

    Components added

--- shopiverse/nuxt.config.ts
+++ shopiverse/nuxt.config.ts
@@ -1,22 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   modules: [
     '@nuxtjs/tailwindcss',
     'nuxt-icon',
     '@nuxtjs/supabase'
   ],
+
   runtimeConfig: {
     stripeSK: process.env.STRIPE_SK_KEY,
     public: {
       stripePK: process.env.STRIPE_PK_KEY
     }
   },
+
   supabase: {
     url: process.env.SUPABASE_URL,
     key: process.env.SUPABASE_KEY,
     redirect: false
   },
+
   app: {
     head: {
       title: 'Shopiverse',
@@ -37,5 +41,7 @@ export default defineNuxtConfig({
         }
       ]
     }
-  }
+  },
+
+  compatibilityDate: '2024-08-17'
 })
---
+++ shopiverse/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div v-if="products && products.length" class="grid grid-cols-4 gap-4">
+      <div v-for="product in products" :key="product.id">
+          <ProductComp :product="product"/>
+      </div>
+  </div>
+</template>
+
+<script setup>
+  const {data: products} = await useFetch('/api/products')
+</script>
